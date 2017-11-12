8383ffe6bd7b63c4a5a703d49961b53cd8a8d7a7
Pablo <pabarch2017@gmail.com>
2017-11-12 19:00:50 -0300

    Add basic modules

---
+++ config/default.js
@@ -0,0 +1,5 @@
+module.exports = {
+    api: {
+        baseUrl: 'https://api.mercadolibre.com'
+    }
+};
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('YESSSS');
+const config = require('config');
+const request = require('request-promise-native');
+
+const httpClient = request.defaults({
+    baseUrl: config.get('api.baseUrl'),
+    forever: true,
+    json: true,
+    timeout: 5000,
+    gzip: true
+});
+
+
+httpClient({
+    url: '/items/MLA619067400'
+}).then(response => {
+   console.log(response);
+}).catch(err => {
+    console.log('Error: ', err);
+});
