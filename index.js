c0905594b2d7714973013db753bd3b97472ceb13
Pablo <pabarch2017@gmail.com>
2017-11-13 22:32:26 -0300

    Add cache module dependency wpi ML Api client

--- index.js
+++ index.js
@@ -3,19 +3,40 @@
 const config = require('config');
 const request = require('request-promise-native');
 
+const MLApiClient = require('./lib/MLApiClient');
+
 const httpClient = request.defaults({
     baseUrl: config.get('api.baseUrl'),
     forever: true,
     json: true,
     timeout: 5000,
-    gzip: true
+    gzip: true,
+    simple: false,
+    resolveWithFullResponse: true
 });
 
 
-httpClient({
-    url: '/items/MLA619067400'
-}).then(response => {
-   console.log(response);
-}).catch(err => {
-    console.log('Error: ', err);
-});
+const apiClient = new MLApiClient(httpClient);
+
+//apiClient.getResource('/items', 'MLA619067400', { 'ETag': '7aa0fec78e43e91427c29a00facb6026'})
+
+const Cacheman = require('cacheman');
+const CachemanFile = require('cacheman-file');
+
+const cacheOtions = {
+    ttl: 20,
+    engine: new CachemanFile({}),
+    Promise: Promise
+};
+
+const cache = new Cacheman('todo', cacheOtions);
+
+cache.get('hola')
+.then(val => {
+    console.log(val);
+})
+
+cache.set('hola', 'chau')
+.then(val => {
+    console.log(val);
+})
