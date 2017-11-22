971f51fefbb5c112f9c673762e8c5e9071c53e4f
Pablo <pabarch2017@gmail.com>
2017-11-21 21:34:05 -0300

    Output results to file

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 'use strict';
 
 const ioc = require('./lib/ioc');
+const fs = require('fs');
 const log = require('winston');
 
-ioc.create('paymentMethodService')
-    .then(paymentMethodService => {
-        return paymentMethodService.find();
+const FILE = './output.txt';
+const productId = process.argv[2] || 'MLA620354952';
+
+ioc.create('productService')
+    .then(productService => {
+        log.info(`Finding product ${productId}...`);
+        return productService.findById(productId);
     })
     .then(product => {
-        log.info(product.toString());
+        ioc.create('paymentMethodService')
+            .then(paymentMethodService => {
+                log.info('Finding payment Methods...');
+                return paymentMethodService.find();
+            })
+            .then(paymentMethods => {
+                let output = `${product}\n`;
+
+                for (const paymentMethod of paymentMethods) {
+                    output += `${paymentMethod}\n`;
+                }
+
+                log.info(`Writing results to file "${FILE}"`);
+                fs.writeFileSync(FILE, output);
+            });
     })
     .catch(err => {
         log.error(err);
